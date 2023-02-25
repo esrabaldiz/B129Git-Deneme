cb0f1e0c5c2668fceab510e9d9816b901ad6d9fa
EsraBaldiz <esracagan85@gmail.com>
2023-02-25 16:51:25 +0100

    V3

--- src/main/java/deneme/Deneme01.java
+++ src/main/java/deneme/Deneme01.java
@@ -3,10 +3,6 @@ package deneme;
 public class Deneme01 {
     public static void main(String[] args) {
         System.out.println("merhaba");
-        System.out.println("dunya");
 
-
-//restore tamamen workin spaceí temizler.restore yaparsak geri getirme sansimiz olmaz
-// stash ise --> orayi hafizaya alir yani geri geitrme sansim var
     }
 }
