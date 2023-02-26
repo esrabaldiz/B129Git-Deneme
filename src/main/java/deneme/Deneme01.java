3fd0c0886df1cdd2505aef92a4f40e989b9b9bd5
EsraBaldiz <esracagan85@gmail.com>
2023-02-26 15:02:35 +0100

    versiyon2

--- src/main/java/deneme/Deneme01.java
+++ src/main/java/deneme/Deneme01.java
@@ -4,6 +4,8 @@ public class Deneme01 {
     public static void main(String[] args) {
         System.out.println("merhaba");
         System.out.println("dunya");
+        System.out.println("B129");
+        System.out.println("yenilik");
 
 
 //restore tamamen workin spaceí temizler.restore yaparsak geri getirme sansimiz olmaz
